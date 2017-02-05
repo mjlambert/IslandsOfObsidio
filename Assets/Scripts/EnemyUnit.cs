c52c52374791abe9097b3ee658b0a525e7e0e8ca
mjlambert <mjlambert55@gmail.com>
2017-02-05 22:24:26 +0800

    add basic scripts for enemy units + boat

---
+++ Assets/Scripts/EnemyUnit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyUnit : MonoBehaviour {
+
+    private NavMeshAgent navMeshAgent;
+
+
+    /// <summary>
+    /// Set destination on the nav mesh agent
+    /// </summary>
+    /// <param name="target">Location of destination.</param>
+    public void SetDestination(Vector3 target)
+    {
+        navMeshAgent.SetDestination(target);
+    }
+
+    public void OnWaypointEnter(Vector3 waypointPosition)
+    {
+        SetDestination(waypointPosition);
+    }
+
+    // Use this for initialization
+    void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        SetDestination(new Vector3(-2.5f, 0f, 0f));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour {
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "EnemyUnit")
+        {
+            EnemyUnit enemyUnit = other.GetComponentInParent<EnemyUnit>();
+            if (enemyUnit == null)
+            {
+                Debug.LogError(string.Format("No EnemyUnit script found on object[{0}]", other.name));
+            }
+            else
+            {
+                enemyUnit.OnWaypointEnter(gameObject.transform.position);
+            }
+        }
+
+    }
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
